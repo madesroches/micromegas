cc57f4a42b27402bb97fd0ee448438a55d06849b
Marc-Antoine Desroches <marc-antoine.desroches@ubisoft.com>
2024-03-14 14:26:49 -0400

    trim admin cli to bare minimum

--- rust/telemetry-admin-cli/src/lake_size.rs
+++ rust/telemetry-admin-cli/src/lake_size.rs
@@ -1,49 +1,7 @@
 use anyhow::Result;
 use lgn_blob_storage::BlobStorage;
-use sqlx::Executor;
 use sqlx::Row;
 use std::sync::Arc;
-use micromegas_tracing::prelude::*;
-
-#[allow(clippy::cast_possible_wrap)]
-pub async fn fill_block_sizes(
-    connection: &mut sqlx::PgConnection,
-    blob_storage: Arc<dyn BlobStorage>,
-) -> Result<()> {
-    let rows = connection
-        .fetch_all(
-            "SELECT blocks.block_id, length(payloads.payload) as in_db_size
-             FROM blocks
-             LEFT JOIN payloads ON blocks.block_id = payloads.block_id
-             WHERE payload_size IS NULL",
-        )
-        .await?;
-
-    let mut nb = 0;
-    for r in rows {
-        let block_id: String = r.try_get("block_id")?;
-        let mut blob_size: Option<i64> = r.try_get("in_db_size")?;
-        if blob_size.is_none() {
-            if let Some(stats) = blob_storage.get_blob_info(&block_id).await? {
-                blob_size = Some(stats.size as i64);
-            } else {
-                error!("blob not found for block {}", block_id);
-            }
-        }
-        if blob_size.is_some() {
-            let size = blob_size.unwrap();
-            println!("{} {}", block_id, size);
-            sqlx::query("UPDATE blocks set payload_size = ? WHERE block_id = ?")
-                .bind(size)
-                .bind(block_id)
-                .execute(&mut *connection)
-                .await?;
-        }
-        nb += 1;
-    }
-    println!("{} rows", nb);
-    Ok(())
-}
 
 pub async fn delete_old_blocks(
     connection: &mut sqlx::PgConnection,
