1cf846319e1f67d679de44b4d45e3a0ca4c7d4e6
madesroches <madesroches@gmail.com>
2024-03-15 16:59:32 -0400

    release batch file

--- build/release.bat
+++ build/release.bat
@@ -1,4 +1,31 @@
 pushd ..\rust
 set PUBLISH_GRACE_SLEEP=2
-cargo release --exclude analytics-srv --exclude telemetry-ingestion-srv --exclude telemetry-admin
+
+cargo release -p micromegas-derive-transit
+@if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
+
+cargo release -p micromegas-transit
+@if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
+
+cargo release -p micromegas-tracing-proc-macros
+@if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
+
+cargo release -p micromegas-tracing
+@if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
+
+cargo release -p micromegas-telemetry
+@if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
+
+cargo release -p micromegas-ingestion
+@if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
+
+cargo release -p micromegas-analytics
+@if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
+
+cargo release -p micromegas-telemetry-sink
+@if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
+
+cargo release -p micromegas
+@if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
+
 popd
