10927d6741d42bbc0514664f6cacff76b4f2aa4d
madesroches <madesroches@gmail.com>
2024-04-11 21:10:39 -0400

    bump version (#60)

--- build/release.bat
+++ build/release.bat
@@ -1,31 +1,31 @@
 pushd ..\rust
 set PUBLISH_GRACE_SLEEP=60
 
-cargo release -p micromegas-derive-transit
+cargo release -p micromegas-derive-transit -x --no-confirm
 @if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
 
-cargo release -p micromegas-transit
+cargo release -p micromegas-transit -x --no-confirm
 @if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
 
-cargo release -p micromegas-tracing-proc-macros
+cargo release -p micromegas-tracing-proc-macros -x --no-confirm
 @if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
 
-cargo release -p micromegas-tracing
+cargo release -p micromegas-tracing -x --no-confirm
 @if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
 
-cargo release -p micromegas-telemetry
+cargo release -p micromegas-telemetry -x --no-confirm
 @if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
 
-cargo release -p micromegas-ingestion
+cargo release -p micromegas-ingestion -x --no-confirm
 @if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
 
-cargo release -p micromegas-telemetry-sink
+cargo release -p micromegas-telemetry-sink -x --no-confirm
 @if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
 
-cargo release -p micromegas-analytics
+cargo release -p micromegas-analytics -x --no-confirm
 @if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
 
-cargo release -p micromegas
+cargo release -p micromegas -x --no-confirm
 @if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
 
 popd
