e969869099b5ff0338ba764d1ce458cec3ee9f03
Marc-Antoine Desroches <marc-antoine.desroches@ubisoft.com>
2024-04-24 12:01:47 -0400

    move serialization to service

--- rust/analytics/src/analytics_service.rs
+++ rust/analytics/src/analytics_service.rs
@@ -1,6 +1,9 @@
 use anyhow::{Context, Result};
 use bytes::Buf;
-use datafusion::arrow::record_batch::RecordBatch;
+use bytes::BufMut;
+use datafusion::parquet::basic::Compression;
+use datafusion::parquet::file::properties::WriterProperties;
+use datafusion::{arrow::record_batch::RecordBatch, parquet::arrow::ArrowWriter};
 use micromegas_ingestion::data_lake_connection::DataLakeConnection;
 use serde::Deserialize;
 
@@ -21,7 +24,7 @@ impl AnalyticsService {
         Self { data_lake }
     }
 
-    pub async fn query_processes(&self, body: bytes::Bytes) -> Result<RecordBatch> {
+    pub async fn query_processes(&self, body: bytes::Bytes) -> Result<bytes::Bytes> {
         let request: QueryProcessesRequest = ciborium::from_reader(body.reader())
             .with_context(|| "parsing QueryProcessesRequest")?;
         let mut connection = self.data_lake.db_pool.acquire().await?;
@@ -33,6 +36,20 @@ impl AnalyticsService {
         .bind(request.limit)
         .fetch_all(&mut *connection)
         .await?;
-        rows_to_record_batch(&rows)
+        serialize_record_batch(
+            &rows_to_record_batch(&rows).with_context(|| "converting rows to record batch")?,
+        )
     }
+}
+
+fn serialize_record_batch(record_batch: &RecordBatch) -> Result<bytes::Bytes> {
+    let mut buffer_writer = bytes::BytesMut::with_capacity(1024).writer();
+    let props = WriterProperties::builder()
+        .set_compression(Compression::LZ4_RAW)
+        .build();
+    let mut arrow_writer =
+        ArrowWriter::try_new(&mut buffer_writer, record_batch.schema(), Some(props))?;
+    arrow_writer.write(record_batch)?;
+    arrow_writer.close()?;
+    Ok(buffer_writer.into_inner().into())
 }
