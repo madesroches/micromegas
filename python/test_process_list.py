42c98fdc8154e34e410c76bc747ea7d3ef157a4a
Marc-Antoine Desroches <marc-antoine.desroches@ubisoft.com>
2024-04-25 09:12:02 -0400

    use pytest for running python integration tests

--- python/test_process_list.py
+++ python/test_process_list.py
@@ -6,20 +6,21 @@ import pyarrow.parquet as pq
 import requests
 import tabulate
 
-end = datetime.datetime.now(datetime.timezone.utc)
-begin = end - datetime.timedelta(days=7)
-end = end + datetime.timedelta(hours=1)
-args = {"limit": 1024, "begin": begin.isoformat(), "end": end.isoformat()}
-body = cbor2.dumps(args)
-response = requests.post(
-    "http://localhost:8082/analytics/query_processes",
-    data=body,
-)
-if response.status_code != 200:
-    raise Exception(response.text)
-table = pq.read_table(io.BytesIO(response.content))
-print(table.schema)
-df = table.to_pandas()
-df = df[ ["process_id", "exe", "username", "start_time", "insert_time"] ]
-print(tabulate.tabulate(df, headers='keys'))
-      
+     
+def test_process_list():
+    end = datetime.datetime.now(datetime.timezone.utc)
+    begin = end - datetime.timedelta(days=7)
+    end = end + datetime.timedelta(hours=1)
+    args = {"limit": 1024, "begin": begin.isoformat(), "end": end.isoformat()}
+    body = cbor2.dumps(args)
+    response = requests.post(
+        "http://localhost:8082/analytics/query_processes",
+        data=body,
+    )
+    if response.status_code != 200:
+        raise Exception(response.text)
+    table = pq.read_table(io.BytesIO(response.content))
+    print(table.schema)
+    df = table.to_pandas()
+    df = df[ ["process_id", "exe", "username", "start_time", "insert_time"] ]
+    print(tabulate.tabulate(df, headers='keys'))
