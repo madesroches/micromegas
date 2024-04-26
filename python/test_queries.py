fc0d1f66a35ce7cb145a7117865268c5da5d676e
Marc-Antoine Desroches <marc-antoine.desroches@ubisoft.com>
2024-04-26 15:21:01 -0400

    start of python lib

--- python/test_queries.py
+++ python/test_queries.py
@@ -1,32 +1,16 @@
 #!/usr/bin/python3
-import cbor2
+import micromegas
+
 import datetime
-import io
-import pyarrow.parquet as pq
 import pandas as pd
-import requests
 import tabulate
+
 
 ANALYTICS_BASE_URL = "http://localhost:8082/analytics/"
 
 
-def request(url_tail, args):
-    url = ANALYTICS_BASE_URL + url_tail
-    response = requests.post(
-        url,
-        data=cbor2.dumps(args),
-    )
-    if response.status_code != 200:
-        raise Exception(
-            "http request url={2} failed with code={0} text={1}".format(
-                response.status_code, response.text, url
-            )
-        )
-    table = pq.read_table(io.BytesIO(response.content))
-    return table.to_pandas()
-
-
 def req(url_tail, args={}):
+    url = ANALYTICS_BASE_URL + url_tail
     # add default args that make sense for tests but would not in general
     if "begin" not in args:
         # set a very large time span if there is not already one specified
@@ -37,7 +21,7 @@ def req(url_tail, args={}):
         args["end"] = end.isoformat()
     if "limit" not in args:
         args["limit"] = 1024
-    return request(url_tail, args)
+    return micromegas.request.request(url, args)
 
 
 def test_process_list():
@@ -55,12 +39,10 @@ def test_find_cpu_stream():
     df = req("query_streams", args={"tag_filter": "cpu"})
     print(df)
 
-
-def test_find_cpu_blocks():
+def get_cpu_strams_with_data():
     streams_df = req("query_streams", args={"tag_filter": "cpu"})
     streams_stats = {}
     for index, row in streams_df.iterrows():
-        print(row["stream_id"])
         blocks_df = req("query_blocks", args={"stream_id": row["stream_id"]})
         if len(blocks_df) == 0:
             stats = {"sum_payload": 0, "nb_events": 0}
@@ -72,4 +54,8 @@ def test_find_cpu_blocks():
         streams_stats[row["stream_id"]] = stats
     streams_stats = pd.DataFrame(streams_stats).transpose()
     streams_stats = streams_stats[streams_stats["nb_events"] > 0]
+    return streams_stats
+
+def test_find_cpu_blocks():
+    streams_stats = get_cpu_strams_with_data()
     print(streams_stats.sort_values("nb_events", ascending=False))
