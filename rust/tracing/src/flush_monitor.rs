2c96d1dd2a665194b17e69245d5bb6a8f9394717
Marc-Antoine Desroches <marc-antoine.desroches@ubisoft.com>
2024-05-23 13:44:34 -0400

    tick the flush monitor (#116)

--- rust/tracing/src/flush_monitor.rs
+++ rust/tracing/src/flush_monitor.rs
@@ -9,21 +9,25 @@ use crate::dispatch::{flush_log_buffer, flush_metrics_buffer, for_each_thread_st
 //   Thread streams can't be flushed without introducing a synchronization mechanism. Their capacity is reduced so that the calling code will flush them in a safe manner.
 pub struct FlushMonitor {
     last_flush: AtomicI64,
-    flush_period: u32,
+    flush_period_seconds: i64,
 }
 
 impl FlushMonitor {
-    pub fn new(flush_period: u32) -> Self {
+    pub fn new(flush_period_seconds: i64) -> Self {
         Self {
             last_flush: AtomicI64::new(Local::now().timestamp()),
-            flush_period,
+            flush_period_seconds,
         }
     }
 
-    pub fn tick(&self) {
+    pub fn time_to_flush_seconds(&self) -> i64 {
         let now = Local::now().timestamp();
-        let diff = now - self.last_flush.load(Ordering::Relaxed);
-        if diff > i64::from(self.flush_period) {
+        let seconds_since_flush = now - self.last_flush.load(Ordering::Relaxed);
+        self.flush_period_seconds - seconds_since_flush
+    }
+
+    pub fn tick(&self) {
+        if self.time_to_flush_seconds() <= 0 {
             if let Some(sink) = get_sink() {
                 if sink.is_busy() {
                     return;
