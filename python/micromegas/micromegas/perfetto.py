8a765b35c2aa0022084591a6964a05d53b3c1436
Marc-Antoine Desroches <marc-antoine.desroches@ubisoft.com>
2024-06-18 09:30:06 -0400

    warn about partial data

--- python/micromegas/micromegas/perfetto.py
+++ python/micromegas/micromegas/perfetto.py
@@ -1,21 +1,26 @@
 import crc
 
+
 # hack to allow perfetto proto imports
 # you can then import the protos like this: from protos.perfetto.trace import trace_pb2
 def load_perfetto_protos():
     import sys
     import pathlib
-    perfetto_folder =  pathlib.Path(__file__).parent.absolute() / "thirdparty/perfetto"
+
+    perfetto_folder = pathlib.Path(__file__).parent.absolute() / "thirdparty/perfetto"
     sys.path.append(str(perfetto_folder))
+
 
 def crc64_str(s):
     calculator = crc.Calculator(crc.Crc64.CRC64)
     return calculator.checksum(str.encode(s))
 
+
 class Writer:
-    def __init__( self, client, process_id, exe ):
+    def __init__(self, client, process_id, exe):
         load_perfetto_protos()
         from protos.perfetto.trace import trace_pb2, trace_packet_pb2
+
         self.client = client
         self.trace = trace_pb2.Trace()
         self.packets = self.trace.packet
@@ -27,8 +32,9 @@ class Writer:
         packet.track_descriptor.process.process_name = exe
         self.packets.append(packet)
 
-    def append_thread( self, begin, end, stream_id, thread_name, thread_id ):
+    def append_thread(self, begin, end, stream_id, thread_name, thread_id):
         from protos.perfetto.trace import trace_pb2, trace_packet_pb2, track_event
+
         packet = trace_packet_pb2.TracePacket()
         thread_uuid = crc64_str(stream_id)
         packet.track_descriptor.uuid = thread_uuid
@@ -39,15 +45,24 @@ class Writer:
         self.packets.append(packet)
         trusted_packet_sequence_id = 1
 
-        df_events = self.client.query_thread_events(begin, end, limit=1024*1024, stream_id = stream_id)
-        df_events["ns"] = df_events["timestamp"].astype('int64')
+        df_events = self.client.query_thread_events(
+            begin, end, limit=1024 * 1024, stream_id=stream_id
+        )
+        nb_events = df_events.shape[0]
+        if nb_events == 1024*1024:
+            print("Warning: partial data returned, needs multiple requests")
+        df_events["ns"] = df_events["timestamp"].astype("int64")
         for index, event in df_events.iterrows():
             packet = trace_packet_pb2.TracePacket()
             packet.timestamp = event["ns"]
             if event["event_type"] == "begin":
-                packet.track_event.type = track_event.track_event_pb2.TrackEvent.Type.TYPE_SLICE_BEGIN
+                packet.track_event.type = (
+                    track_event.track_event_pb2.TrackEvent.Type.TYPE_SLICE_BEGIN
+                )
             elif event["event_type"] == "end":
-                packet.track_event.type = track_event.track_event_pb2.TrackEvent.Type.TYPE_SLICE_END
+                packet.track_event.type = (
+                    track_event.track_event_pb2.TrackEvent.Type.TYPE_SLICE_END
+                )
             else:
                 raise Exception("unknown event type")
             packet.track_event.track_uuid = thread_uuid
@@ -58,10 +73,6 @@ class Writer:
             packet.trusted_packet_sequence_id = trusted_packet_sequence_id
             self.packets.append(packet)
 
-    def write_file( self, filename ):
+    def write_file(self, filename):
         with open(filename, "wb") as f:
             f.write(self.trace.SerializeToString())
-        
-        
-
-        
