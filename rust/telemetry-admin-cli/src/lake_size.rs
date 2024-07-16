772a05cce8a1816ec0926f6ceef3f3d071091945
Marc-Antoine Desroches <marc-antoine.desroches@ubisoft.com>
2024-07-16 11:23:17 -0400

    deleting expired streams & processes (#155)
    * delete expired streams
    
    * deleting expired processes

--- rust/telemetry-admin-cli/src/lake_size.rs
+++ rust/telemetry-admin-cli/src/lake_size.rs
@@ -1,15 +1,16 @@
-use anyhow::Result;
-use micromegas::chrono::Days;
-use micromegas::chrono::Utc;
+use anyhow::{Context, Result};
+use chrono::{DateTime, Days, Utc};
+use micromegas::chrono;
 use micromegas::ingestion::data_lake_connection::DataLakeConnection;
 use micromegas::sqlx::{query, Row};
 use micromegas::tracing::prelude::*;
 use micromegas::uuid::Uuid;
 
 // returns true if there is more data to delete
-pub async fn delete_old_blocks_batch(lake: &DataLakeConnection, min_days_old: i32) -> Result<bool> {
-    let now = Utc::now();
-    let expiration = now.checked_sub_days(Days::new(min_days_old as u64));
+pub async fn delete_expired_blocks_batch(
+    lake: &DataLakeConnection,
+    expiration: DateTime<Utc>,
+) -> Result<bool> {
     let batch_size: i32 = 1000;
     let rows = query(
         "SELECT process_id, stream_id, block_id
@@ -40,7 +41,113 @@ pub async fn delete_old_blocks_batch(lake: &DataLakeConnection, min_days_old: i3
     Ok(paths.len() == batch_size as usize)
 }
 
-pub async fn delete_old_blocks(lake: &DataLakeConnection, min_days_old: i32) -> Result<()> {
-    while delete_old_blocks_batch(lake, min_days_old).await? {}
+pub async fn delete_expired_blocks(
+    lake: &DataLakeConnection,
+    expiration: DateTime<Utc>,
+) -> Result<()> {
+    while delete_expired_blocks_batch(lake, expiration).await? {}
+    Ok(())
+}
+
+pub async fn delete_empty_streams_batch(
+    lake: &DataLakeConnection,
+    expiration: DateTime<Utc>,
+) -> Result<bool> {
+    let batch_size: i32 = 1000;
+    // it would be more efficient to just run a delete statetement, but I want to log what gets deleted and why
+    // in the future, we could also delete the associated caches
+    let rows = query(
+        "SELECT streams.stream_id
+         FROM streams
+         LEFT OUTER JOIN blocks ON blocks.stream_id = streams.stream_id
+         WHERE streams.insert_time <= $1
+         GROUP BY streams.stream_id
+         HAVING count(blocks.block_id) = 0
+         LIMIT $2;",
+    )
+    .bind(expiration)
+    .bind(batch_size)
+    .fetch_all(&lake.db_pool)
+    .await?;
+    let mut stream_ids = vec![];
+    for r in rows {
+        let stream_id: Uuid = r.try_get("stream_id")?;
+        stream_ids.push(stream_id);
+    }
+
+    info!("deleting expired streams {stream_ids:?}");
+    query("DELETE FROM streams where stream_id = ANY($1);")
+        .bind(&stream_ids)
+        .execute(&lake.db_pool)
+        .await?;
+
+    Ok(stream_ids.len() == batch_size as usize)
+}
+
+pub async fn delete_empty_streams(
+    lake: &DataLakeConnection,
+    expiration: DateTime<Utc>,
+) -> Result<()> {
+    while delete_empty_streams_batch(lake, expiration).await? {}
+    Ok(())
+}
+
+pub async fn delete_empty_processes_batch(
+    lake: &DataLakeConnection,
+    expiration: DateTime<Utc>,
+) -> Result<bool> {
+    let batch_size: i32 = 1000;
+    // it would be more efficient to just run a delete statetement, but I want to log what gets deleted and why
+    // in the future, we could also delete the associated caches
+    let rows = query(
+        "SELECT processes.process_id
+         FROM processes
+         LEFT OUTER JOIN streams ON streams.process_id = processes.process_id
+         WHERE processes.insert_time <= $1
+         GROUP BY processes.process_id
+         HAVING count(streams.stream_id) = 0
+         LIMIT $2;",
+    )
+    .bind(expiration)
+    .bind(batch_size)
+    .fetch_all(&lake.db_pool)
+    .await?;
+    let mut process_ids = vec![];
+    for r in rows {
+        let process_id: Uuid = r.try_get("process_id")?;
+        process_ids.push(process_id);
+    }
+
+    info!("deleting expired processes {process_ids:?}");
+    query("DELETE FROM processes where process_id = ANY($1);")
+        .bind(&process_ids)
+        .execute(&lake.db_pool)
+        .await?;
+
+    Ok(process_ids.len() == batch_size as usize)
+}
+
+pub async fn delete_empty_processes(
+    lake: &DataLakeConnection,
+    expiration: DateTime<Utc>,
+) -> Result<()> {
+    while delete_empty_processes_batch(lake, expiration).await? {}
+    Ok(())
+}
+
+pub async fn delete_old_data(lake: &DataLakeConnection, min_days_old: i32) -> Result<()> {
+    let now = Utc::now();
+    let expiration = now
+        .checked_sub_days(Days::new(min_days_old as u64))
+        .with_context(|| "computing expiration date/time")?;
+    delete_expired_blocks(lake, expiration)
+        .await
+        .with_context(|| "delete_expired_blocks")?;
+    delete_empty_streams(lake, expiration)
+        .await
+        .with_context(|| "delete_empty_streams")?;
+    delete_empty_processes(lake, expiration)
+        .await
+        .with_context(|| "delete_empty_processes")?;
     Ok(())
 }
