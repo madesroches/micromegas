90ff872cf5f634abb000dbf467a34e81e1e62d5b
Marc-Antoine Desroches <marc-antoine.desroches@ubisoft.com>
2024-07-23 14:12:31 -0400

    move out of date partitions to the temp files table

--- rust/analytics/src/lakehouse/batch_update.rs
+++ rust/analytics/src/lakehouse/batch_update.rs
@@ -58,8 +58,7 @@ async fn write_partition(
     .bind(&partition_metadata.source_data_hash)
     .execute(&mut *tr)
     .await
-		.with_context(|| "inserting into lakehouse_partitions")?;
-
+    .with_context(|| "inserting into lakehouse_partitions")?;
 
     let old_partitions = sqlx::query(
         "SELECT file_path, file_size
@@ -76,10 +75,41 @@ async fn write_partition(
     .bind(partition_metadata.begin_insert_time)
     .bind(partition_metadata.end_insert_time)
     .bind(&partition_metadata.source_data_hash)
-    .fetch_all(&lake.db_pool)
+    .fetch_all(&mut *tr)
     .await
-    .with_context(|| "listing up to date partitions")?;
-	
+    .with_context(|| "listing old partitions")?;
+    for old_part in old_partitions {
+        let file_path: String = old_part.try_get("file_path")?;
+        let file_size: i64 = old_part.try_get("file_size")?;
+        let expiration = Utc::now() + TimeDelta::try_hours(1).with_context(|| "making one hour")?;
+        info!("adding out of date partition {file_path} to temporary files to be deleted");
+        sqlx::query("INSERT INTO temporary_files VALUES ($1, $2, $3);")
+            .bind(file_path)
+            .bind(file_size)
+            .bind(expiration)
+            .execute(&mut *tr)
+            .await
+            .with_context(|| "adding old partition to temporary files to be deleted")?;
+    }
+
+    sqlx::query(
+        "DELETE from lakehouse_partitions
+         WHERE table_set_name = $1
+         AND table_instance_id = $2
+         AND begin_insert_time = $3
+         AND end_insert_time = $4
+         AND source_data_hash != $5
+         ;",
+    )
+    .bind(&partition_metadata.table_set_name)
+    .bind(&partition_metadata.table_instance_id)
+    .bind(partition_metadata.begin_insert_time)
+    .bind(partition_metadata.end_insert_time)
+    .bind(&partition_metadata.source_data_hash)
+    .execute(&mut *tr)
+    .await
+    .with_context(|| "deleting out of date partitions")?;
+
     tr.commit().await.with_context(|| "commit")?;
     Ok(())
 }
