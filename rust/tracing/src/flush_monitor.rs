f2c9ac6c19276f1a58283f6273e40a8239d47991
Marc-Antoine Desroches <marc-antoine.desroches@ubisoft.com>
2024-07-31 11:30:36 -0400

    flush, busy or not

--- rust/tracing/src/flush_monitor.rs
+++ rust/tracing/src/flush_monitor.rs
@@ -5,7 +5,6 @@ use crate::dispatch::{flush_log_buffer, flush_metrics_buffer, for_each_thread_st
 
 // FlushMonitor triggers the flush of the telemetry streams every minute.
 //   Must be ticked.
-//   Will not flush the streams if the sink is busy writing.
 //   Thread streams can't be flushed without introducing a synchronization mechanism. Their capacity is reduced so that the calling code will flush them in a safe manner.
 pub struct FlushMonitor {
     last_flush: AtomicI64,
@@ -28,13 +27,6 @@ impl FlushMonitor {
 
     pub fn tick(&self) {
         if self.time_to_flush_seconds() <= 0 {
-            if let Some(sink) = get_sink() {
-                if sink.is_busy() {
-                    return;
-                }
-            } else {
-                return;
-            }
             self.last_flush
                 .store(Local::now().timestamp(), Ordering::Relaxed);
             flush_log_buffer();
