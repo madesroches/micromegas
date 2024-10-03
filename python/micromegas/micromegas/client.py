da89c2399eefdbc7ccc2eb2cf90a3251431887b8
Marc-Antoine Desroches <marc-antoine.desroches@ubisoft.com>
2024-10-03 09:34:59 -0400

    remove old python api (#235)

--- python/micromegas/micromegas/client.py
+++ python/micromegas/micromegas/client.py
@@ -22,17 +22,6 @@ class Client:
             headers=self.headers,
         )
 
-    def query_processes(self, begin, end, limit):
-        return request.request(
-            self.analytics_base_url + "query_processes",
-            {
-                "begin": time.format_datetime(begin),
-                "end": time.format_datetime(end),
-                "limit": limit,
-            },
-            headers=self.headers,
-        )
-
     def query_streams(self, begin, end, limit, process_id=None, tag_filter=None):
         args = {
             "begin": time.format_datetime(begin),
@@ -59,63 +48,6 @@ class Client:
         return request.request(
             self.analytics_base_url + "query_blocks",
             args,
-            headers=self.headers,
-        )
-
-    def query_spans(self, begin, end, limit, stream_id):
-        return request.request(
-            self.analytics_base_url + "query_spans",
-            {
-                "begin": time.format_datetime(begin),
-                "end": time.format_datetime(end),
-                "limit": limit,
-                "stream_id": stream_id,
-            },
-            headers=self.headers,
-        )
-
-    def query_thread_events(self, begin, end, limit, stream_id):
-        return request.request(
-            self.analytics_base_url + "query_thread_events",
-            {
-                "begin": time.format_datetime(begin),
-                "end": time.format_datetime(end),
-                "limit": limit,
-                "stream_id": stream_id,
-            },
-            headers=self.headers,
-        )
-
-    def query_log_entries(
-        self,
-        begin,
-        end,
-        limit=None,  # Necessary if stream_id is specified, ignored otherwise
-        stream_id=None,  # If none, query is run on cached lakehouse using query engine
-        sql=None,  # Necessary if stream_id is None, ignored otherwise
-    ):
-        return request.request(
-            self.analytics_base_url + "query_log_entries",
-            {
-                "begin": time.format_datetime(begin),
-                "end": time.format_datetime(end),
-                "limit": limit,
-                "stream_id": stream_id,
-                "sql": sql,
-            },
-            headers=self.headers,
-        )
-
-    def query_metrics(self, begin, end, limit=None, stream_id=None, sql=None):
-        return request.request(
-            self.analytics_base_url + "query_metrics",
-            {
-                "begin": time.format_datetime(begin),
-                "end": time.format_datetime(end),
-                "limit": limit,
-                "stream_id": stream_id,
-                "sql": sql,
-            },
             headers=self.headers,
         )
 
