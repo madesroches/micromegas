c2b39c88b34e98b80fd69c705c3d4cd1f9535594
madesroches <madesroches@gmail.com>
2025-01-05 14:55:22 -0500

    docker image publication, scripts to run the services (#275)
    * publish docker container
    
    * docker_command
    
    * run ingestion container
    
    * run flightsql server
    
    * add telemetry-admin to docker image
    
    * run cron container

---
+++ build/run_cron_container.py
@@ -0,0 +1,10 @@
+#!/bin/python3
+import docker_command
+import os
+
+os.environ["MICROMEGAS_TELEMETRY_URL"] = "http://localhost:9000"
+os.environ["MICROMEGAS_OBJECT_STORE_URI"] = "file:///lake"
+
+docker_command.run_docker_command(
+    "docker run --network=host -v ~/lake:/lake -e MICROMEGAS_TELEMETRY_URL -e MICROMEGAS_SQL_CONNECTION_STRING -e MICROMEGAS_OBJECT_STORE_URI -d marcantoinedesroches/micromegas:0.3 /telemetry-admin crond",
+)
