4dbef9e28e9725ce0273a5c0cad29560292a1053
madesroches <madesroches@gmail.com>
2025-02-05 19:13:39 -0500

    Release (#298)
    * release script
    
    * version bump

---
+++ build/release.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+import os
+from rust_command import run_command
+
+os.environ["PUBLISH_GRACE_SLEEP"] = "60"
+
+run_command("cargo fmt --check")
+run_command("cargo release -p micromegas-derive-transit -x --no-confirm")
+run_command("cargo release -p micromegas-transit -x --no-confirm")
+run_command("cargo release -p micromegas-tracing-proc-macros -x --no-confirm")
+run_command("cargo release -p micromegas-tracing -x --no-confirm")
+run_command("cargo release -p micromegas-telemetry -x --no-confirm")
+run_command("cargo release -p micromegas-ingestion -x --no-confirm")
+run_command("cargo release -p micromegas-telemetry-sink -x --no-confirm")
+run_command("cargo release -p micromegas-analytics -x --no-confirm")
+run_command("cargo release -p micromegas -x --no-confirm")
