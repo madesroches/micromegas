30a9a57d24fb895fb2d1d6496bc4da6a9058f055
Marc-Antoine Desroches <marc-antoine.desroches@ubisoft.com>
2025-03-06 16:20:33 -0500

    cargo lock. fmt

--- rust/perfetto/src/update_perfetto_protos.rs
+++ rust/perfetto/src/update_perfetto_protos.rs
@@ -1,7 +1,7 @@
 use anyhow::Result;
 
 fn main() -> Result<()> {
-	std::env::set_var("OUT_DIR", "perfetto/src/");
+    std::env::set_var("OUT_DIR", "perfetto/src/");
     let proto_include_paths = vec!["F:/git/external/perfetto/"];
     let protos = vec!["protos/perfetto/trace/trace.proto"];
     prost_build::compile_protos(&protos, &proto_include_paths)?;
