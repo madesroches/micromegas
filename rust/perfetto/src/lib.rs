72f53b0df29e1ee5b899ca02281c9ad054931c04
madesroches <madesroches@gmail.com>
2025-04-04 16:39:54 -0400

    clippy warnings (#339)

--- rust/perfetto/src/lib.rs
+++ rust/perfetto/src/lib.rs
@@ -5,7 +5,8 @@
 #[allow(
     clippy::doc_lazy_continuation,
     clippy::len_without_is_empty,
-    clippy::large_enum_variant
+    clippy::large_enum_variant,
+    clippy::doc_overindented_list_items
 )]
 #[cfg(not(feature = "protogen"))]
 pub mod protos {
