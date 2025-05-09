8c01fe95a399ec8fb701844c7530b51d03eff036
Marc-Antoine Desroches <marc-antoine.desroches@ubisoft.com>
2025-05-09 09:31:02 -0400

    module comment

--- rust/analytics/src/dfext/histogram/mod.rs
+++ rust/analytics/src/dfext/histogram/mod.rs
@@ -4,4 +4,5 @@ pub mod quantile;
 /// Histogram data structures and aggregate function
 pub mod histogram_udaf;
 
+/// Compute variance from running sum and sum of squares in the histogram
 pub mod variance;
