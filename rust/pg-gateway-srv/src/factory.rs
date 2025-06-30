5a75caf5c9f84c604ee512fcebe690cbe65ff5e8
Marc-Antoine Desroches <marc-antoine.desroches@ubisoft.com>
2025-06-30 10:41:54 -0400

    send server parameters on startup

--- rust/pg-gateway-srv/src/factory.rs
+++ rust/pg-gateway-srv/src/factory.rs
@@ -1,65 +1,10 @@
 use crate::simple::SimpleQueryH;
+use crate::startup::StartupH;
 use crate::state::ConnectionState;
 use crate::{extended::NullExtendedQueryHandler, state::SharedState};
-use async_trait::async_trait;
-use futures::sink::{Sink, SinkExt};
-use micromegas::client::flightsql_client_factory::BearerFlightSQLClientFactory;
-use micromegas::tracing::info;
-use pgwire::{
-    api::{
-        auth::StartupHandler, copy::NoopCopyHandler, ClientInfo, NoopErrorHandler,
-        PgWireConnectionState, PgWireServerHandlers,
-    },
-    error::{PgWireError, PgWireResult},
-    messages::{
-        response::{ReadyForQuery, TransactionStatus},
-        startup::Authentication,
-        PgWireBackendMessage, PgWireFrontendMessage,
-    },
-};
-use std::{fmt::Debug, sync::Arc};
+use pgwire::api::{copy::NoopCopyHandler, NoopErrorHandler, PgWireServerHandlers};
+use std::sync::Arc;
 use tokio::sync::Mutex;
-
-pub struct StartupH {
-    state: SharedState,
-}
-
-impl StartupH {
-    pub fn new(state: SharedState) -> Self {
-        Self { state }
-    }
-}
-
-#[async_trait]
-impl StartupHandler for StartupH {
-    /// A generic frontend message callback during startup phase.
-    async fn on_startup<C>(
-        &self,
-        client: &mut C,
-        message: PgWireFrontendMessage,
-    ) -> PgWireResult<()>
-    where
-        C: ClientInfo + Sink<PgWireBackendMessage> + Unpin + Send,
-        C::Error: Debug,
-        PgWireError: From<<C as Sink<PgWireBackendMessage>>::Error>,
-    {
-        info!("on_startup message={message:?}");
-        self.state
-            .lock()
-            .await
-            .set_factory(Arc::new(BearerFlightSQLClientFactory::new("".into())));
-        client.set_state(PgWireConnectionState::ReadyForQuery);
-        client
-            .send(PgWireBackendMessage::Authentication(Authentication::Ok))
-            .await?;
-        client
-            .send(PgWireBackendMessage::ReadyForQuery(ReadyForQuery::new(
-                TransactionStatus::Idle,
-            )))
-            .await?;
-        Ok(())
-    }
-}
 
 pub struct HandlerFactory {
     state: SharedState,
