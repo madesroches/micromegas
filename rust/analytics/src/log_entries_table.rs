943f2c5045b519e5d10eac6a4f8144b7c32d38f9
Marc-Antoine Desroches <madesroches@gmail.com>
2025-07-30 20:33:09 -0400

    fixed doc

--- rust/analytics/src/log_entries_table.rs
+++ rust/analytics/src/log_entries_table.rs
@@ -101,7 +101,6 @@ pub fn log_table_schema() -> Schema {
 }
 
 /// A builder for creating a `RecordBatch` of log entries.
-/// A builder for creating a `RecordBatch` of log entries.
 pub struct LogEntriesRecordBuilder {
     process_ids: StringDictionaryBuilder<Int16Type>,
     stream_ids: StringDictionaryBuilder<Int16Type>,
