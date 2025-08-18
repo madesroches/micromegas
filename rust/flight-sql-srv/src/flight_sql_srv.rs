0df454cf577d87098aa6bfbc356b239fab2c18c5
Marc-Antoine Desroches <madesroches@gmail.com>
2025-08-18 16:00:15 -0400

    Reduce verbosity in ingestion and analytics services
    Set interop_max_level to "info" to reduce noise from external dependencies while keeping debug-level logging for application code.

--- rust/flight-sql-srv/src/flight_sql_srv.rs
+++ rust/flight-sql-srv/src/flight_sql_srv.rs
@@ -26,7 +26,7 @@ struct Cli {
     disable_auth: bool,
 }
 
-#[micromegas_main(max_level_override = "debug")]
+#[micromegas_main(interop_max_level = "info", max_level_override = "debug")]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Cli::parse();
     let connection_string = std::env::var("MICROMEGAS_SQL_CONNECTION_STRING")
