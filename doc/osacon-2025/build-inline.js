428c88a616a4e3f8717a79a0930628e5f23157af
Marc-Antoine Desroches <madesroches@gmail.com>
2025-10-07 11:14:57 -0400

    Fix presentation title

--- doc/osacon-2025/build-inline.js
+++ doc/osacon-2025/build-inline.js
@@ -26,7 +26,7 @@ const inlineHtml = `<!DOCTYPE html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Technical Presentation Template</title>
+    <title>Micromegas - Unified Observability for Video Games - OSACON 2025</title>
     <style>
 ${mainCss}
     </style>
