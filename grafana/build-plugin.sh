b3e22276cd3dd8a619bef20c60480b096ff6ef2b
Marc-Antoine Desroches <madesroches@gmail.com>
2025-11-24 13:58:06 -0500

    Fix Grafana plugin packaging and document release process
    - Make build-plugin.sh idempotent by cleaning up old artifacts
    - Add release documentation to grafana/README.md
    - Prevents nested dist/ folder issue in plugin packages
    
    The build script now removes existing build artifacts before
    packaging, ensuring consistent structure regardless of dirty state.

--- grafana/build-plugin.sh
+++ grafana/build-plugin.sh
@@ -4,5 +4,6 @@ yarn install --pure-lockfile
 yarn build
 mage -v
 mage build:generateManifestFile
+rm -rf micromegas-micromegas-datasource micromegas-micromegas-datasource.zip
 cp -r dist micromegas-micromegas-datasource
 zip -r micromegas-micromegas-datasource.zip micromegas-micromegas-datasource
