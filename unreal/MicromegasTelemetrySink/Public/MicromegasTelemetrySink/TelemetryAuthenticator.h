97d69f2a53c365680755228ec9744a35546c34c8
Marc-Antoine Desroches <marc-antoine.desroches@ubisoft.com>
2025-12-03 09:19:39 -0500

    Add API key authenticator to Unreal telemetry sink (#618)
    Adds FApiKeyAuthenticator for simple Bearer token authentication.
    Also adds Shutdown() method to ITelemetryAuthenticator interface.

--- unreal/MicromegasTelemetrySink/Public/MicromegasTelemetrySink/TelemetryAuthenticator.h
+++ unreal/MicromegasTelemetrySink/Public/MicromegasTelemetrySink/TelemetryAuthenticator.h
@@ -12,8 +12,9 @@ class MICROMEGASTELEMETRYSINK_API ITelemetryAuthenticator
 public:
 	virtual ~ITelemetryAuthenticator() = 0;
 	virtual void Init(const MicromegasTracing::EventSinkPtr& InSink) = 0;
+	virtual void Shutdown();
 	virtual bool IsReady() = 0;
-	virtual bool Sign(IHttpRequest& request) = 0;
+	virtual bool Sign(IHttpRequest& Request) = 0;
 };
 
 typedef TSharedPtr<ITelemetryAuthenticator, ESPMode::ThreadSafe> SharedTelemetryAuthenticator;
