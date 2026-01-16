dfd42b488919c76fbfd2ac8e5c43ff3f1d4c909d
Marc-Antoine Desroches <madesroches@gmail.com>
2026-01-16 16:28:03 -0500

    Fix page title flickering on navigation
    Remove cleanup function that was resetting title to app name on unmount,
    causing a brief flicker when navigating between pages.

--- analytics-web-app/src/hooks/usePageTitle.ts
+++ analytics-web-app/src/hooks/usePageTitle.ts
@@ -4,14 +4,6 @@ const APP_NAME = 'Micromegas'
 
 export function usePageTitle(title: string | undefined | null): void {
   useEffect(() => {
-    if (title) {
-      document.title = `${title} - ${APP_NAME}`
-    } else {
-      document.title = APP_NAME
-    }
-
-    return () => {
-      document.title = APP_NAME
-    }
+    document.title = title ? `${title} - ${APP_NAME}` : APP_NAME
   }, [title])
 }
