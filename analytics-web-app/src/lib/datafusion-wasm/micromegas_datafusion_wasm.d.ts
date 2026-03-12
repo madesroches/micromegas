37bc801ce990c6d8cb2624602721b9f9fc030146
Marc-Antoine Desroches <marc-antoine.desroches@ubisoft.com>
2026-03-12 12:08:08 -0400

    update wasm js

--- analytics-web-app/src/lib/datafusion-wasm/micromegas_datafusion_wasm.d.ts
+++ analytics-web-app/src/lib/datafusion-wasm/micromegas_datafusion_wasm.d.ts
@@ -57,9 +57,9 @@ export interface InitOutput {
     readonly rust_zstd_wasm_shim_memmove: (a: number, b: number, c: number) => number;
     readonly rust_zstd_wasm_shim_memset: (a: number, b: number, c: number) => number;
     readonly rust_zstd_wasm_shim_qsort: (a: number, b: number, c: number, d: number) => void;
-    readonly wasm_bindgen__closure__destroy__hbe28d9cc5661c8be: (a: number, b: number) => void;
-    readonly wasm_bindgen__convert__closures_____invoke__h04fdd830bb54d5e4: (a: number, b: number, c: any, d: any) => void;
-    readonly wasm_bindgen__convert__closures_____invoke__h738387317060838d: (a: number, b: number, c: any) => void;
+    readonly wasm_bindgen__closure__destroy__hb68ae3098241167a: (a: number, b: number) => void;
+    readonly wasm_bindgen__convert__closures_____invoke__h68aee7af6a5a44dc: (a: number, b: number, c: any, d: any) => void;
+    readonly wasm_bindgen__convert__closures_____invoke__h157c52e499d8a1e8: (a: number, b: number, c: any) => void;
     readonly __wbindgen_exn_store: (a: number) => void;
     readonly __externref_table_alloc: () => number;
     readonly __wbindgen_externrefs: WebAssembly.Table;
