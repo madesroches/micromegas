024b5e3d723d9b0d79b5d781fa85910251f42696
Marc-Antoine Desroches <madesroches@gmail.com>
2026-05-26 16:37:44 -0400

    update wasm files

--- analytics-web-app/src/lib/datafusion-wasm/micromegas_datafusion_wasm.d.ts
+++ analytics-web-app/src/lib/datafusion-wasm/micromegas_datafusion_wasm.d.ts
@@ -57,7 +57,7 @@ export interface InitOutput {
     readonly rust_zstd_wasm_shim_memmove: (a: number, b: number, c: number) => number;
     readonly rust_zstd_wasm_shim_memset: (a: number, b: number, c: number) => number;
     readonly rust_zstd_wasm_shim_qsort: (a: number, b: number, c: number, d: number) => void;
-    readonly wasm_bindgen__closure__destroy__h973cc31d7fb437d2: (a: number, b: number) => void;
+    readonly wasm_bindgen__closure__destroy__h394910442e9fa771: (a: number, b: number) => void;
     readonly wasm_bindgen__convert__closures_____invoke__haf48848aa6f4ecee: (a: number, b: number, c: any) => [number, number];
     readonly wasm_bindgen__convert__closures_____invoke__h41f8af0b13ef8fb0: (a: number, b: number, c: any, d: any) => void;
     readonly __wbindgen_exn_store: (a: number) => void;
--- analytics-web-app/src/lib/datafusion-wasm/micromegas_datafusion_wasm.js
+++ analytics-web-app/src/lib/datafusion-wasm/micromegas_datafusion_wasm.js
@@ -184,8 +184,8 @@ function __wbg_get_imports() {
             return ret;
         },
         __wbindgen_cast_0000000000000001: function(arg0, arg1) {
-            // Cast intrinsic for `Closure(Closure { dtor_idx: 102, function: Function { arguments: [Externref], shim_idx: 28513, ret: Result(Unit), inner_ret: Some(Result(Unit)) }, mutable: true }) -> Externref`.
-            const ret = makeMutClosure(arg0, arg1, wasm.wasm_bindgen__closure__destroy__h973cc31d7fb437d2, wasm_bindgen__convert__closures_____invoke__haf48848aa6f4ecee);
+            // Cast intrinsic for `Closure(Closure { dtor_idx: 108, function: Function { arguments: [Externref], shim_idx: 30709, ret: Result(Unit), inner_ret: Some(Result(Unit)) }, mutable: true }) -> Externref`.
+            const ret = makeMutClosure(arg0, arg1, wasm.wasm_bindgen__closure__destroy__h394910442e9fa771, wasm_bindgen__convert__closures_____invoke__haf48848aa6f4ecee);
             return ret;
         },
         __wbindgen_cast_0000000000000002: function(arg0, arg1) {
